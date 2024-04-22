cfad3470311ce787c7f7e64886ca0a33d4b2c898
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 00:45:12 +0200

    Update settings.py

--- medicaldashboard/medicaldashboard/settings.py
+++ medicaldashboard/medicaldashboard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-20y_yirorffvm+vxxelh-oeqc-6kl4)pf@8=p3&x6bc3mn9v45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['RealtimeMedicalDashboard.pythonanywhere.com']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
