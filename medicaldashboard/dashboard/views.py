d09e4647ccca9af8428db51e39c52dc9022ecf0e
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-24 01:46:45 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-import pandas as pd
+from .models import DataEntry
 
 def home(request):
-    return render(request, 'dashboard/home.html')
+    letters = DataEntry.objects.values_list('letter', flat=True).distinct()
+    selected_letter = request.GET.get('letter')
+    entries = DataEntry.objects.filter(letter=selected_letter) if selected_letter else DataEntry.objects.none()
+    return render(request, 'home.html', {'letters': letters, 'entries': entries, 'selected_letter': selected_letter})
+
 #/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv
