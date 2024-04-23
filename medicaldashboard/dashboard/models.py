391be087d59898657525c856d94ca812b2a8fa6f
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-24 01:43:37 +0200

    Update models.py

--- medicaldashboard/dashboard/models.py
+++ medicaldashboard/dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DataEntry(models.Model):
+    value = models.IntegerField()
+    color = models.CharField(max_length=100)
+    letter = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f"{self.letter} - {self.value} - {self.color}"
