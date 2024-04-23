7fc7820bf1d8b21275e86c4b52d947e958fe2428
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 03:18:29 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -2,7 +2,7 @@ import csv
 from django.http import HttpResponse
 
 def load_data_from_csv(request):
-    path = 'data.csv'
+    path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data.csv'
     with open(path, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         data = [row for row in reader]
