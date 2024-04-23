3aff5ea257f2b512031d22cc59d32acd71524372
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 13:21:08 +0200

    Update urls.py

--- medicaldashboard/dashboard/urls.py
+++ medicaldashboard/dashboard/urls.py
@@ -3,5 +3,5 @@ from .views import home, load_csv_data
 
 urlpatterns = [
     path('', home, name='home'),
-    path('load-csv/', load_csv_data, name='load-csv'),
+    path('load-csv/', load_csv_data, name='load_csv_data'),
 ]
