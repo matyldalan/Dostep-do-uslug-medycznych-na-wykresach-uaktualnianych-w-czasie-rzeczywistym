34c6929c53022dba9b31955c3fca0f98c4154e03
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 03:31:42 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -1,9 +1,4 @@
-import csv
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def load_data_from_csv(request):
-    path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data.csv'
-    with open(path, newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        data = [row for row in reader]
-    return HttpResponse(data)
+def home(request):
+    return render(request, 'dashboard/home.html')
