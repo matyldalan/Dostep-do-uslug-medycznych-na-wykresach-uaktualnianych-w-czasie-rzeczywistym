f02f8b8f58f5e32eee638543b9595897238102e5
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 11:12:14 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import pandas as pd
 
-def home(request):
-    return render(request, 'dashboard/home.html')
+def load_csv_data(request):
+    file_path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv'
+    data = pd.read_csv(file_path)
+    context = {
+        'data': data.to_html(classes='table table-striped', index=False)
+    }
+    return render(request, 'your_template_name.html', context)
