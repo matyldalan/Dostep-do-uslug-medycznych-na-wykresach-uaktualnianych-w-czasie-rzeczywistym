0681d094313f14c2e222d56b656c834d8d897aba
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 03:57:24 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from django.http import HttpResponse
 
 def load_csv_data(request):
     file_path = 'data/data.csv'
     data = pd.read_csv(file_path)
-    return HttpResponse("Data loaded successfully, number of rows: {}".format(len(data)))
