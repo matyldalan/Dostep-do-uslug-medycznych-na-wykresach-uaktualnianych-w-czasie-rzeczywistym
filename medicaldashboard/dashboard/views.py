d433ac56186c1a0cef16e90ca269411c3f66cfeb
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 13:51:05 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -6,25 +6,22 @@ def home(request):
     return render(request, 'dashboard/home.html')
 
 def load_csv_data(request):
+    file_path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv'  # Zmień na odpowiednią ścieżkę
+    data = pd.read_csv(file_path)
+    letters = list(data['Letter'].unique())
+
     if request.method == 'POST':
         selected_letter = request.POST.get('letter_choice')
-        file_path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv'
-        data = pd.read_csv(file_path)
-
         filtered_data = data[data['Letter'] == selected_letter]
-
         context = {
             'data': filtered_data[['ID', 'Value', 'Color']].to_html(classes='table table-striped', index=False),
-            'letters': list(data['Letter'].unique()),
+            'letters': letters,
             'selected_letter': selected_letter
         }
-        return render(request, 'home.html', context)
     else:
-        file_path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv'
-        data = pd.read_csv(file_path)
         context = {
             'data': '',
-            'letters': list(data['Letter'].unique()),
-            'selected_letter': None
+            'letters': letters,
+            'selected_letter': 'A'
         }
-        return render(request, 'home.html', context)
+    return render(request, 'home.html', context)
