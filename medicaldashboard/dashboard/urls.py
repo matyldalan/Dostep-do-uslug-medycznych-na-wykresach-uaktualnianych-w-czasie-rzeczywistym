45518e61c59fb31821abab7e03dfa090444731e3
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 03:51:58 +0200

    Update urls.py

--- medicaldashboard/dashboard/urls.py
+++ medicaldashboard/dashboard/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import home, load_csv_data, filter_data
+from .views import home
 
 urlpatterns = [
     path('', home, name='home'),
-    path('load-csv/', load_csv_data, name='load-csv'),
-    path('', filter_data, name='filter-data'),
 ]
