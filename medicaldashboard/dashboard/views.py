12afaa08d58a8e9910c9523e097339b72423b707
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-23 13:36:58 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -3,7 +3,23 @@ from django.http import HttpResponse
 import pandas as pd
 
 def home(request):
-    return render(request, 'dashboard/home.html')
+    file_path = '/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv'
+    data = pd.read_csv(file_path)
+    letters = data['Letter'].unique().tolist()
+
+    context = {
+        'letters': letters,
+        'selected_letter': None,
+        'data': ''
+    }
+
+    if request.method == 'POST':
+        selected_letter = request.POST.get('letter_choice')
+        filtered_data = data[data['Letter'] == selected_letter]
+        context['data'] = filtered_data[['ID', 'Value', 'Color']].to_html(classes='table table-striped', index=False)
+        context['selected_letter'] = selected_letter
+
+    return render(request, 'home.html', context)
 
 def load_csv_data(request):
     if request.method == 'POST':
