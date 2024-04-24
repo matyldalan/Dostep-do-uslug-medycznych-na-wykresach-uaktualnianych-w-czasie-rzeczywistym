58d82071d817dcb7376fcc1aed6714051aa73e8f
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-24 02:03:04 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -5,6 +5,6 @@ def home(request):
     letters = DataEntry.objects.values_list('letter', flat=True).distinct()
     selected_letter = request.GET.get('letter')
     entries = DataEntry.objects.filter(letter=selected_letter) if selected_letter else DataEntry.objects.none()
-    return render(request, 'home.html', {'letters': letters, 'entries': entries, 'selected_letter': selected_letter})
+    return render(request, 'dashboard/home.html', {'letters': letters, 'entries': entries, 'selected_letter': selected_letter})
 
 #/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv
