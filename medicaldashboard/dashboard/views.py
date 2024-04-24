1ed902f36fca97865e8e998b360d3d63892a30e8
JuliaKordek <150183188+JuliaKordek@users.noreply.github.com>
2024-04-24 02:30:27 +0200

    Update views.py

--- medicaldashboard/dashboard/views.py
+++ medicaldashboard/dashboard/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
 from .models import DataEntry
+import json
 
 def home(request):
     letters = DataEntry.objects.values_list('letter', flat=True).distinct()
     selected_letter = request.GET.get('letter')
     entries = DataEntry.objects.filter(letter=selected_letter) if selected_letter else DataEntry.objects.none()
-    return render(request, 'dashboard/home.html', {'letters': letters, 'entries': entries, 'selected_letter': selected_letter})
+    entries_json = json.loads(serialize('json', entries))
+    return render(request, 'dashboard/home.html', {
+        'letters': letters,
+        'entries': entries_json,
+        'selected_letter': selected_letter
+    })
 
 #/home/RealtimeMedicalDashboard/Dostep-do-uslug-medycznych-na-wykresach-uaktualnianych-w-czasie-rzeczywistym/medicaldashboard/data/data.csv
